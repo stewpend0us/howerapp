ddaaeb66ecccf1fe2e19d25344772e2a20ceda4b
stewart <stewart.wadsworth@gmail.com>
2022-02-17 22:23:29 -0500

    try es2015 (lambdas at least)

--- spack.config.js
+++ spack.config.js
@@ -14,7 +14,7 @@ module.exports = config({
 /*            minify: {
                 compress: true,
             } */
-            target: "es3"
+            target: "es2015"
         }
     }
 });
