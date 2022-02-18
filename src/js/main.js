7e4d97223f8ef3dc8a024ea3e013f388014b8721
stewart <stewart.wadsworth@gmail.com>
2022-02-18 00:06:20 -0500

    remove some and no lambda in the bluetooth object

--- src/js/main.js
+++ src/js/main.js
@@ -11,11 +11,10 @@ const puseragent = document.getElementById("useragent");
 
 // handlers
 //window.addEventListener("load", startup);
-if (screen.orientation){
+if (screen.orientation) {
   screen.orientation.addEventListener("change", setOrientation);
 }
-else
-{
+else {
   window.addEventListener("orientationchange", setOrientationIOS);
 }
 connectbutton.addEventListener("click", askUserToConnect);
@@ -38,7 +37,7 @@ function updateStatus(e) {
 // change layout depending on orientation
 function setOrientation() {
   let value = "";
-  updateStatus(screen.orientation);
+  updateStatus(screen.orientation.angle);
   switch (screen.orientation.type) {
     case "landscape-primary": value = "row"; break;
     case "landscape-secondary": value = "row-reverse"; break;
@@ -83,8 +82,9 @@ function askUserToConnect() {
   navigator.bluetooth.requestDevice(options).then(device => {
     BLEDevice = device;
     updateStatus("Connecting...");
-    BLEDevice.addEventListener("gattserverdisconnected", () => handleDisconnect("Device disconnected."));
-    BLEDevice.gatt.connect().then(server => {
+    BLEDevice.addEventListener("gattserverdisconnected", handleDisconnect);
+    BLEDevice.addEventListener('gattserverdisconnected', onDisconnected);
+    BLEDevice.gatt.connect();/*.then(server => {
       GATTServer = server;
       GATTServer.getPrimaryService(UART.service).then(service => {
         UARTService = service;
@@ -105,54 +105,55 @@ function askUserToConnect() {
       });
     });
   }).catch(handleDisconnect);
-  
- updateStatus("Connected.");
+ */
+    updateStatus("Connected.");
+  }
 }
 
 function handleDisconnect(ev) {
-  updateStatus(ev);
-  BLEDevice = undefined;
-  GATTServer = undefined;
-  UARTService = undefined;
-  UARTRx = undefined;
-  UARTTx = undefined;
+      updateStatus(ev);
+      BLEDevice = undefined;
+      GATTServer = undefined;
+      UARTService = undefined;
+      UARTRx = undefined;
+      UARTTx = undefined;
 
-  connectbutton.classList.remove("hidden");
-  upbutton.classList.add("hidden");
-  downbutton.classList.add("hidden");
-}
+      connectbutton.classList.remove("hidden");
+      upbutton.classList.add("hidden");
+      downbutton.classList.add("hidden");
+    }
 
 function handleUartRx(ev) {
-  ev.message = arraybuffer2str(ev.currentTarget.value.buffer);
-  updateStatus(ev);
-}
+      ev.message = arraybuffer2str(ev.currentTarget.value.buffer);
+      updateStatus(ev);
+    }
 
 // https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
 // look into "TextEncoder"?
 function arraybuffer2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
-}
+      return String.fromCharCode.apply(null, new Uint8Array(buf));
+    }
 
 function str2arraybuffer(str) {
-  let buf = new ArrayBuffer(str.length); // 2 bytes for each char
-  let bufView = new Uint8Array(buf);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
+      let buf = new ArrayBuffer(str.length); // 2 bytes for each char
+      let bufView = new Uint8Array(buf);
+      for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
+    }
 
 upbutton.addEventListener("click", () => {
-  updateStatus("UP");
-  //await UARTTx.writeValue(str2arraybuffer("Go up!\n"));
-});
+      updateStatus("UP");
+      //await UARTTx.writeValue(str2arraybuffer("Go up!\n"));
+    });
 
-downbutton.addEventListener("click", () => {
-  updateStatus("DOWN");
-  //await UARTTx.writeValue(str2arraybuffer("Go down!\n"));
-});
+  downbutton.addEventListener("click", () => {
+    updateStatus("DOWN");
+    //await UARTTx.writeValue(str2arraybuffer("Go down!\n"));
+  });
 
-updatebutton.addEventListener("click", () => {
-  updateStatus("UPDATE");
-});
+  updatebutton.addEventListener("click", () => {
+    updateStatus("UPDATE");
+  });
 
