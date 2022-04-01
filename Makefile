1a97e07ed67a3ad09a1dd8c94471ac2c96b3f23e
Stewart <stewart.wadsworth@gmail.com>
2022-03-31 21:45:42 -0400

    done with fedora....

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
-.PHONY: all build install clean cert clean-cert
+.PHONY: all build install clean cert clean-cert serve
 
-all: build
+all: build serve
 
 build: clean
 	cp -r src/ build/
 	workbox generateSW workbox-config.js
 
 install:
-	npm install workbox-cli --global
+	sudo npm install workbox-cli --global
 
 clean:
 	rm -rf build/
 
-cert: clean-cert
+cert: key.pem cert.pem
+	
+key.pem cert.pem:
 	openssl req -x509 -nodes -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -subj "/C=US/ST=Oregon/L=Portland/O=Company Name/OU=Org/CN=192.168.1.15"
 
 clean-cert:
--- src/js/main.js
+++ src/js/main.js
@@ -13,187 +13,203 @@ const puseragent = document.getElementById("useragent");
 // handlers
 window.addEventListener("load", startup);
 if (screen.orientation) {
-  screen.orientation.addEventListener("change", setOrientation);
+	screen.orientation.addEventListener("change", setOrientation);
 }
 else {
-  window.addEventListener("orientationchange", setOrientation);
+	window.addEventListener("orientationchange", setOrientation);
 }
 connectbutton.addEventListener("click", askUserToConnect);
 
 function startup() {
-  if ("serviceWorker" in navigator)
-  {
-    navigator.serviceWorker?.register("./sw.js")
-    .catch(err =>{
-    updateStatus("no service worker :(");
-    });
-  }
-  if ("bluetooth" in navigator)
-  {
-  }
-  else
-  {
-    updateStatus("no bluetooth :(");
-  }
-  setOrientation();
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("./sw.js")
+			.catch(err => {
+				updateStatus(err);
+			});
+	}
+	if ("bluetooth" in navigator) {
+	}
+	else {
+		updateStatus("no bluetooth :(");
+	}
+	setOrientation();
 }
 
 // general stuff
 function updateStatus(e) {
-  console.log(e);
-  statusmessage.textContent = e.message ?? String(e);
+	console.log(e);
+	statusmessage.textContent += "\n" + (e.message ?? String(e));
 }
 
 // change layout depending on orientation
 function setOrientation() {
-  let value = "";
-  switch (screen.orientation?.angle ?? window.orientation) {
-    case 90: value = "row"; break;
-    case -90:
-    case 270: value = "row-reverse"; break;
-    case 0: value = "column"; break;
-    case 180: value = "column-reverse"; break;
-    default: console.log(screen.orientation.type); return;
-  }
-  mainsection.style.flexDirection = value;
+	let value = "";
+	switch (screen.orientation?.angle ?? window.orientation) {
+		case 90: value = "row"; break;
+		case -90:
+		case 270: value = "row-reverse"; break;
+		case 0: value = "column"; break;
+		case 180: value = "column-reverse"; break;
+		default: console.log(screen.orientation.type); return;
+	}
+	mainsection.style.flexDirection = value;
 }
 
 // bluetooth stuff
 const UART = {
-  service: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
-  TX: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
-  RX: "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
+	name: "UART",
+	service: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
+	TX: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
+	RX: "6e400003-b5a3-f393-e0a9-e50e24dcca9e",
 };
 
 const DFU = {
-  service: "00001530-1212-efde-1523-785f3713d123",
-  control: "00001531-1212-efde-1523-785f3713d123",
-  packet:  "00001532-1212-efde-1523-785f3713d123",
-  version: "00001534-1212-efde-1523-785f3713d123"
+	name: "DFU",
+	service: "00001530-1337-eabb-2845-bee366ecee5a",
+	control: "00001531-1337-eabb-2845-bee366ecee5a",
+	packet: "00001532-1337-eabb-2845-bee366ecee5a",
+	version: "00001534-1337-eabb-2845-bee366ecee5a"
 };
+
+function connectTo(obj) {
+	let result = {
+		name: obj.name,
+	};
+	let promises = [];
+	GATTServer?.getPrimaryService(obj.service)
+		.then(service => {
+			updateStatus("Got " + obj.name );
+			result.service = service;
+			for (const prop in obj) {
+				if (prop === "service" || prop === "name") {
+					continue;
+				}
+				promises.push(service.getCharacteristic(obj[prop])
+					.then(char => {
+						updateStatus("Got " + obj.name + "." + prop + " characteristic");
+						result[prop] = char;
+					})
+					.catch(error => {
+						updateStatus("No " + obj.name + "." + prop + " characteristic?");
+						updateStatus(error);
+					}));
+			}
+			//Promise.allSettled(promises);
+			return result;
+		})
+		.catch(error => {
+			updateStatus("no " + obj.name + " service?");
+			updateStatus(error);
+		});
+}
 
 let BLEDevice = undefined;
 let GATTServer = undefined;
-let UARTService = undefined;
-let UARTRx = undefined;
-let UARTTx = undefined;
+let uart = undefined;
+let dfu = undefined;
 
 function askUserToConnect() {
 
-  puseragent.textContent = window.navigator.userAgent;
-  const options = {
-    acceptAllDevices: false,
-    filters: [
-      { namePrefix: "Itsy" },
-      { namePrefix: "AdaDFU" },
-      { namePrefix: "Hower" },
-      { services: [UART.service] },
-      { services: [DFU.service] }
-    ],
-  };
+	puseragent.textContent = window.navigator.userAgent;
+	const options = {
+		acceptAllDevices: false,
+		filters: [
+			{ namePrefix: "Itsy" },
+			{ namePrefix: "AdaDFU" },
+			{ namePrefix: "Hower" },
+			{ services: [UART.service] },
+			{ services: [DFU.service] },
+		],
+	};
 
-  navigator.bluetooth.requestDevice(options)
-    .then(device => {
-      BLEDevice = device;
-      updateStatus("Connecting...");
-      BLEDevice.addEventListener("gattserverdisconnected", handleDisconnect);
-      return BLEDevice.gatt.connect();
-    })
-    .then(server => {
-      updateStatus("Connected.");
-      GATTServer = server;
-      return GATTServer.getPrimaryService(UART.service);
-    })
-    .then(service => {
-      updateStatus("Getting UART Service.");
-      UARTService = service;
-      return UARTService.getCharacteristic(UART.TX);
-    })
-    .then(char => {
-      updateStatus("Getting Tx characteristic.");
-      UARTTx = char;
-      return UARTService.getCharacteristic(UART.RX);
-    })
-    .then(char => {
-      updateStatus("Listening for Rx notifications.");
-      UARTRx = char;
-      UARTRx.addEventListener("characteristicvaluechanged", handleUartRx);
-      UARTRx.startNotifications();
-      return;
-    })
-    .then(() => {
-      updateStatus("Connected.");
-      connectbutton.classList.add("hidden");
-      upbutton.classList.remove("hidden");
-      downbutton.classList.remove("hidden");
-      disconnectbutton.classList.remove("hidden");
-      return;
-    })
-    .catch(handleDisconnect);
+	navigator.bluetooth?.requestDevice(options)
+		.then(device => {
+			BLEDevice = device;
+			updateStatus("Connected to " + device.name);
+			BLEDevice.addEventListener("gattserverdisconnected", handleDisconnect);
+			return BLEDevice.gatt.connect();
+		})
+		.then(server => {
+			updateStatus("Connected to Gatt.");
+			GATTServer = server;
+			return server;
+		})
+		.then(server => {
+			uart = connectTo(UART);
+			dfu = connectTo(DFU);
+			uart.RX.addEventListener("characteristicvaluechanged", handleUartRx);
+			uart.RX.startNotifications();
+		})
+		.finally(() => {
+			updateStatus("Connected.");
+			connectbutton.classList.add("hidden");
+			upbutton.classList.remove("hidden");
+			downbutton.classList.remove("hidden");
+			disconnectbutton.classList.remove("hidden");
+		});
 }
 
 function handleDisconnect(ev) {
-  updateStatus(ev);
-  BLEDevice = undefined;
-  GATTServer = undefined;
-  UARTService = undefined;
-  UARTRx = undefined;
-  UARTTx = undefined;
+	updateStatus("Disconnect Event");
+	BLEDevice = undefined;
+	GATTServer = undefined;
+	uart = undefined;
+	dfu = undefined;
 
-  connectbutton.classList.remove("hidden");
-  upbutton.classList.add("hidden");
-  downbutton.classList.add("hidden");
-  disconnectbutton.classList.add("hidden");
+	connectbutton.classList.remove("hidden");
+	upbutton.classList.add("hidden");
+	downbutton.classList.add("hidden");
+	disconnectbutton.classList.add("hidden");
 }
 
 function handleUartRx(ev) {
-  ev.message = arraybuffer2str(ev.currentTarget.value.buffer);
-  updateStatus(ev);
+	ev.message = arraybuffer2str(ev.currentTarget.value.buffer);
+	updateStatus(ev);
 }
 
 // https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
 // look into "TextEncoder"?
 function arraybuffer2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
+	return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
 function str2arraybuffer(str) {
-  let buf = new ArrayBuffer(str.length); // 2 bytes for each char
-  let bufView = new Uint8Array(buf);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
+	let buf = new ArrayBuffer(str.length); // 2 bytes for each char
+	let bufView = new Uint8Array(buf);
+	for (let i = 0, strLen = str.length; i < strLen; i++) {
+		bufView[i] = str.charCodeAt(i);
+	}
+	return buf;
 }
 
 upbutton.addEventListener("pointerdown", () => {
-  updateStatus("UP");
-  UARTTx?.writeValue(str2arraybuffer("Go up!\n"))
-    .catch(updateStatus);
+	updateStatus("UP");
+	uart?.TX?.writeValue(str2arraybuffer("Go up!\n"))
+		.catch(updateStatus);
 });
 
 upbutton.addEventListener("pointerup", () => {
-  updateStatus("...");
-  UARTTx?.writeValue(str2arraybuffer("no more go up\n"))
-    .catch(updateStatus);
+	updateStatus("...");
+	uart?.TX?.writeValue(str2arraybuffer("no more go up\n"))
+		.catch(updateStatus);
 });
 
 downbutton.addEventListener("pointerdown", () => {
-  updateStatus("DOWN");
-  UARTTx?.writeValue(str2arraybuffer("Go down!\n"))
-    .catch(updateStatus);
+	updateStatus("DOWN");
+	uart?.TX?.writeValue(str2arraybuffer("Go down!\n"))
+		.catch(updateStatus);
 });
 
 downbutton.addEventListener("pointerup", () => {
-  updateStatus("...");
-  UARTTx?.writeValue(str2arraybuffer("no more go down\n"))
-    .catch(updateStatus);
+	updateStatus("...");
+	uart?.TX?.writeValue(str2arraybuffer("no more go down\n"))
+		.catch(updateStatus);
 });
 
 updatebutton.addEventListener("click", () => {
-  updateStatus("UPDATE");
+	updateStatus("UPDATE");
 });
 
 disconnectbutton.addEventListener("click", () => {
-  GATTServer?.disconnect();
+	GATTServer?.disconnect();
 })
