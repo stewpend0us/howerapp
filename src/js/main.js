93696e4cd655ee782e0aee27e6deb770f9a55586
stewart <stewart.wadsworth@gmail.com>
2022-04-01 21:49:09 -0400

    time to implement the actual dfu update logic

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,8 @@ else {
 connectbutton.addEventListener("click", askUserToConnect);
 
 function startup() {
+	updatebutton.classList.remove("hidden");
+	return;
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker.register("./sw.js")
 			.catch(err => {
@@ -78,7 +80,7 @@ function connectTo(obj) {
 	let promises = [];
 	GATTServer?.getPrimaryService(obj.service)
 		.then(service => {
-			updateStatus("Got " + obj.name );
+			updateStatus("Got " + obj.name);
 			result.service = service;
 			for (const prop in obj) {
 				if (prop === "service" || prop === "name") {
@@ -136,16 +138,24 @@ function askUserToConnect() {
 		})
 		.then(server => {
 			uart = connectTo(UART);
-			dfu = connectTo(DFU);
 			uart.RX.addEventListener("characteristicvaluechanged", handleUartRx);
 			uart.RX.startNotifications();
 		})
-		.finally(() => {
+		.then(() => {
 			updateStatus("Connected.");
 			connectbutton.classList.add("hidden");
 			upbutton.classList.remove("hidden");
 			downbutton.classList.remove("hidden");
 			disconnectbutton.classList.remove("hidden");
+		})
+		.catch(error => {
+			dfu = connectTo(DFU);
+			connectbutton.classList.add("hidden");
+			updatebutton.classList.remove("hidden");
+		})
+		.catch(error => {
+			updateStatus("welp that's not good.");
+			updateStatus(error);
 		});
 }
 
@@ -208,8 +218,241 @@ downbutton.addEventListener("pointerup", () => {
 
 updatebutton.addEventListener("click", () => {
 	updateStatus("UPDATE");
+	const dfu = new Dfu(null);
+	console.log(dfu);
 });
 
 disconnectbutton.addEventListener("click", () => {
 	GATTServer?.disconnect();
 })
+
+/// Dfu class:
+function Dfu() {
+	/*
+	Initializes the dfu upgrade, unpacks zip and registers callbacks.
+
+	@param zip_file_path{ Path to the zip file with the firmware to upgrade
+	@type zip_file_path{ str
+	@param connect_delay{ Delay in seconds before each connection to the DFU target
+	@type connect_delay{ int
+	@return
+	*/
+	this.total_size = 0;
+	fetch("/firmware/manifest.json")
+		.then(response => {
+			return response.json();
+		})
+		.then(manifest => {
+			console.log(manifest);
+			this.manifest = manifest.manifest;
+			this.files = {};
+
+
+			Promise.allSettled([
+				this._get_files("softdevice_bootloader"),
+				this._get_files("softdevice"),
+				this._get_files("bootloader"),
+				this._get_files("application"),
+			]);
+		})
+}
+
+Dfu.prototype._get_files = function (name) {
+	return Promise.all([
+		this._get_file(name, "bin_file"),
+		this._get_file(name, "dat_file"),
+	]);
+}
+
+Dfu.prototype._get_file = function (name, type) {
+	// append name to this with the contents of the file as a blob
+	if (name in this.manifest) {
+		const wanted = "firmware/" + this.manifest[name][type];
+		updateStatus("fetching " + wanted);
+		return fetch(wanted)
+			.then(response => {
+				if (response.ok)
+					return response.blob();
+				else
+					throw "response not ok"
+			})
+			.then(data => {
+				if (!(name in this.files)) {
+					this.files[name] = {};
+				}
+				this.files[name][type] = data;
+				if (type === "dat_file") {
+					this.total_size += data.size
+				}
+				updateStatus("fetched " + wanted);
+			})
+			.catch(error => {
+				updateStatus("failed to get " + wanted);
+				updateStatus(error);
+			});
+	}
+}
+
+Dfu.prototype._dfu_send_image = function (firmware) {
+	start_time = Date.now();
+
+	updateStatus("Sending init packet...");
+	this.send_init_packet(this.files[firmware].dat_file);
+
+	updateStatus("Sending firmware file...");
+	this.send_firmware(this.files[firmware].bin_file);
+
+	end_time = Date.now();
+	updateStatus(`Image sent in ${end_time - start_time}`);
+}
+
+Dfu.prototype.send_init_packet = function (init_packet) {
+	function try_to_recover() {
+		if (response['offset'] == 0 || response['offset'] > len(init_packet)) {
+			// There is no init packet or present init packet is too long.
+			return false;
+		}
+
+		expected_crc = (binascii.crc32(init_packet[: response['offset']]) & 0xFFFFFFFF);
+
+		if (expected_crc != response['crc']) {
+			// Present init packet is invalid.
+			return false;
+		}
+
+		if (len(init_packet) > response['offset']) {
+			// Send missing part.
+			try {
+				this.__stream_data(data = init_packet[response['offset']:],
+					crc = expected_crc,
+					offset = response['offset']);
+			}
+			catch (ValidationException) {
+				return false;
+			}
+		}
+
+		this.__execute();
+		return true;
+
+	}
+
+	response = this.__select_command();
+	if (len(init_packet) <= response["max_size"])
+		throw "Init command is too long";
+
+	if (try_to_recover())
+		return;
+
+	for (r in range(DfuTransportBle.RETRIES_NUMBER)) {
+		try {
+			this.__create_command(len(init_packet))
+			this.__stream_data(data = init_packet)
+			this.__execute()
+			return;
+		}
+		finally { }
+	}
+	throw "Failed to send init packet";
+}
+
+Dfu.prototype.send_firmware = function (firmware) {
+	function try_to_recover() {
+		if (response['offset'] == 0) {
+			// Nothing to recover
+			return;
+		}
+
+		expected_crc = binascii.crc32(firmware[: response['offset']]) & 0xFFFFFFFF;
+		remainder = response['offset'] % response['max_size'];
+
+		if (expected_crc != response['crc']) {
+			// Invalid CRC. Remove corrupted data.
+			response['offset'] -= remainder != 0 ? remainder : response['max_size'];
+			response['crc'] = binascii.crc32(firmware[: response['offset']]) & 0xFFFFFFFF;
+			return;
+		}
+
+		if ((remainder != 0) && (response['offset'] != len(firmware))) {
+			// Send rest of the page.
+			try {
+				to_send = firmware[response['offset'] : response['offset'] + response['max_size'] - remainder];
+				response['crc'] = this.__stream_data(data = to_send,
+					crc = response['crc'],
+					offset = response['offset']);
+				response['offset'] += len(to_send);
+			}
+			catch (error) {
+				// Remove corrupted data.
+				response['offset'] -= remainder;
+				response['crc'] = binascii.crc32(firmware[: response['offset']]) & 0xFFFFFFFF;
+				return;
+			}
+		}
+
+		this.__execute();
+		//	this._send_event(event_type=DfuEvent.PROGRESS_EVENT, progress=response['offset']);
+	}
+	response = this.__select_data();
+	try_to_recover();
+
+	for (i in range(response['offset'], len(firmware), response['max_size'])) {
+		data = firmware[i: i + response['max_size']];
+		for (r in range(DfuTransportBle.RETRIES_NUMBER)) {
+			try {
+				this.__create_data(len(data));
+				response['crc'] = this.__stream_data(data = data, crc = response['crc'], offset = i);
+				this.__execute();
+			}
+			finally { }
+			break;
+		}
+		throw "Failed to send firmware";
+		//	this._send_event(event_type=DfuEvent.PROGRESS_EVENT, progress=len(data))
+	}
+}
+
+Dfu.prototype.__select_command = function () {
+	return this.__select_object(0x01);
+}
+
+Dfu.prototype._select_data = function () {
+	return this.__select_object(0x02);
+}
+
+Dfu.prototype.__select_object = function (object_type) {
+	console.log(`BLE: Selecting Object: type:${object_type}`);
+	this.dfu_adapter.write_control_point([DfuTransportBle.OP_CODE['ReadObject'], object_type]);
+	response = this.__get_response(DfuTransportBle.OP_CODE['ReadObject']);
+
+	(max_size, offset, crc) = struct.unpack('<III', bytearray(response));
+	console.log(`BLE: Object selected: max_size:${max_size} offset:${offset} crc:${crc}`);
+	return { "max_size": max_size, "offset": offset, "crc": crc };
+}
+
+Dfu.prototype.dfu_send_images = function () {
+	/*
+	Does DFU for all firmware images in the stored manifest.
+	*/
+	if (this.files.softdevice_bootloader) {
+		updateStatus("Sending SoftDevice+Bootloader image...")
+		this._dfu_send_image("softdevice_bootloader");
+	}
+
+	if (this.files.softdevice) {
+		updateStatus("Sending SoftDevice image...");
+		this._dfu_send_image("softdevice");
+	}
+
+	if (this.files.bootloader) {
+		updateStatus("Sending Bootloader image...");
+		this._dfu_send_image("bootloader");
+	}
+
+	if (this.files.application) {
+		updateStatus("Sending Application image...");
+		this._dfu_send_image("application");
+	}
+}
+
+Dfu.prototype.dfu_get_total_size = () => this.total_size;
